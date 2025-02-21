c4c8c6e99726979d6fb3544cc38dcfd813b60af7
Sahit Mamidipaka <smamidipaka6@gatech.edu>
2025-02-21 11:35:01 -0500

    light version - finalized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Editor } from "@/components/Editor";
 
+// This is the Fastest, Most Light Version of the Site — it's the fastest website in the world (as far as I could find)
+
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-background text-foreground flex flex-col">
