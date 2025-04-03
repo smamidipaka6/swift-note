950bc5e114091a0bee8b04e4c4a8c1364d06a29b
Sahit Mamidipaka <smamidipaka6@gatech.edu>
2025-04-03 15:57:16 -0400

    Color Page.tsx

---
+++ src/app/colors/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ColorTesting from '@/components/ColorTesting'
+
+const page = () => {
+  return (
+    <div className='flex flex-col mt-12 items-center justify-center'>
+      <ColorTesting />
+    </div>
+  )
+}
+
+export default page
